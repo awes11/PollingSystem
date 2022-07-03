19b2ed6e25c3624971e53a9c051a53d1309dfb46
aadarsh111 <aadarsh.rauniyar11@gmail.com>
2022-07-03 11:53:38 +0545

    front end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+import { ConnectButton } from "web3uikit";
+import logo from "./images/poll_logo.png";
+
+import Coin from "./Components/Coin";
+import { useMoralis } from "react-moralis";
+
+
+const App = () => {
+
+  const[poll, setPoll] = useState(50);
+  const[poll2, setPoll2] = useState(50);
+  
+
+  const{Moralis, isInitialized} = useMoralis();
+
+ 
+
+  async function getRatio(tick, setPerc){
+
+    const Votes = Moralis.Object.extend("Votes");
+    
+    const query = new Moralis.Query(Votes);
+    query.equalTo("ticker", tick);
+    query.descending("createdAt");
+    const results = await query.first();
+    let up = Number(results.attributes.up);
+    let down = Number(results.attributes.down);
+    let ratio = Math.round(up/(up+down)*100);
+  
+    setPerc(ratio);
+    
+  }
+
+  useEffect(() => {
+    if(isInitialized){
+    getRatio("Reconstruct Lab", setPoll);
+    getRatio("Nike PartnerShip", setPoll2);
+    
+
+    async function createLiveQuery(){
+      let query = new Moralis.Query('Votes');
+      let subscription = await query.subscribe();
+      subscription.on('update', (object) => {
+        
+        if(object.attributes.ticker === "Reconstruct Lab"){
+          getRatio("Reconstruct Lab", setPoll);
+        }else if(object.attributes.ticker === "Nike PartnerShip"){
+          getRatio("Nike PartnerShip", setPoll2);
+        
+        }
+      });
+    }
+    createLiveQuery();
+    }
+    
+  }, [isInitialized]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <div className="header">
+       <div className="logo">
+         <img src={logo} alt="logo" height="50px" />
+          Poll
+       </div>
+       <ConnectButton />
+     </div>
+     <div className="instructions">
+       Help us make a better decision with your votes 
+     </div>
+     <div className="subinstructions">
+       Vote up or down for the presented polls
+
+     </div>
+     <div className="list">
+       <Coin
+     perc={poll}
+     setPerc={setPoll}
+     token={"Reconstruct Lab"}
+     />
+     <Coin
+     perc={poll2}
+     setPerc={setPoll2}
+     token={"Nike PartnerShip"}
+     />
+    
+     </div>
+     
+    </>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MoralisProvider } from "react-moralis";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <MoralisProvider appId="TSxrQyNQxDPK5mJl0TclkmyJUcnCxje39Yly5mOE" serverUrl="https://dl5anhka0pqe.usemoralis.com:2053/server">
+          <App />
+    </MoralisProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
